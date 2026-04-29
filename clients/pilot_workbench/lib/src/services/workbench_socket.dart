775fc33e65b8e18602b98bf79240080b121d412a
swj <3109114823@qq.com>
2026-04-29 13:19:59 +0800

    fix：修复客户端ws重复连接问题

--- clients/pilot_workbench/lib/src/services/workbench_socket.dart
+++ clients/pilot_workbench/lib/src/services/workbench_socket.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WorkbenchSocket {
+  static const Duration heartbeatInterval = Duration(seconds: 25);
+
   SocketConnection connect({
     required Uri uri,
     required void Function(Map<String, dynamic> event) onEvent,
@@ -11,6 +13,29 @@ class WorkbenchSocket {
     required void Function(Object error) onError,
   }) {
     final channel = WebSocketChannel.connect(uri);
+    Timer? heartbeatTimer;
+    var isClosed = false;
+
+    void stopHeartbeat() {
+      heartbeatTimer?.cancel();
+      heartbeatTimer = null;
+    }
+
+    void sendPing() {
+      if (isClosed) {
+        return;
+      }
+      try {
+        channel.sink.add('ping');
+      } catch (error) {
+        isClosed = true;
+        stopHeartbeat();
+        onError(error);
+      }
+    }
+
+    heartbeatTimer = Timer.periodic(heartbeatInterval, (_) => sendPing());
+
     final subscription = channel.stream.listen(
       (data) {
         if (data is String) {
@@ -20,11 +45,29 @@ class WorkbenchSocket {
           }
         }
       },
-      onDone: onClosed,
-      onError: onError,
+      onDone: () {
+        if (isClosed) {
+          return;
+        }
+        isClosed = true;
+        stopHeartbeat();
+        onClosed();
+      },
+      onError: (Object error) {
+        if (isClosed) {
+          return;
+        }
+        isClosed = true;
+        stopHeartbeat();
+        onError(error);
+      },
       cancelOnError: false,
     );
-    return SocketConnection(channel: channel, subscription: subscription);
+    return SocketConnection(
+      channel: channel,
+      subscription: subscription,
+      stopHeartbeat: stopHeartbeat,
+    );
   }
 }
 
@@ -32,12 +75,15 @@ class SocketConnection {
   SocketConnection({
     required this.channel,
     required this.subscription,
+    required this.stopHeartbeat,
   });
 
   final WebSocketChannel channel;
   final StreamSubscription<dynamic> subscription;
+  final void Function() stopHeartbeat;
 
   Future<void> close() async {
+    stopHeartbeat();
     await subscription.cancel();
     await channel.sink.close();
   }
